c2021cde245e0a7d1b17aea941a98fe042916340
mansoorishaq1 <77729021+mansoorishaq1@users.noreply.github.com>
2021-02-03 23:11:24 +0500

    Update queue.js

--- queue.js
+++ queue.js
@@ -4,36 +4,25 @@ var myRealQueue=[];
 function addtoQueue(value){
     myQueue.push(value);
 }
-// function removeQueue(){
-//     for(let i=myQueue.length;i>0;i--){
-//         myRealQueue.push(myQueue.pop());
-//         console.log(myRealQueue);
-//     }
-//     myRealQueue.pop();
-//     for(let i=myRealQueue.length;i>0;i--){
-//         myQueue.push(myRealQueue.pop());
-//     }
-// }
-        function removeQueue(){
-            for(let i=0;i<=myQueue.length;i++){
-                myRealQueue.push(myQueue.pop());
-            }
-            console.log(myRealQueue);
-            myRealQueue.pop();
-           
-        }
+function removeQueue(){
+    for(let i=myQueue.length;i>0;i--){
+        myRealQueue.push(myQueue.pop());
+     
+    }
+    myRealQueue.pop();
+    for(let i=myRealQueue.length;i>0;i--){
+        myQueue.push(myRealQueue.pop());
+    }
+}
 addtoQueue(1);
 addtoQueue(2);
 addtoQueue(3);
-// addtoQueue(4);
-// addtoQueue(5);
-// addtoQueue(6);
+addtoQueue(4);
+addtoQueue(5);
+addtoQueue(6);
   removeQueue();
 //  removeQueue();
-//  addtoQueue(7);
+//  addtoQueue();
 //  removeQueue();
-//console.log(myQueue);
-//console.log(myRealQueue);
-
-
+console.log(myQueue);
 
