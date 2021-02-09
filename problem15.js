f28c061a8ecb8b5868ab1e4e7d504b3f5a5b232f
Basit Ali <37082492+basitk41@users.noreply.github.com>
2021-02-10 01:11:06 +0500

    Problem 15 Solution

--- problem15.js
+++ problem15.js
@@ -1,3 +1,7 @@
+//Problem 15:- make a function "delayedUpperCase(input, n)" that takes in a 2 parameters and returns a new promise.
+//using setTimeout, after n milliseconds, the promise will either resolove or reject. if the input is a string, 
+//the promise resolves with that same string uppercased. if the input is anything but a string it rejects with that same input
+
 delayedUpperCase = (input,n) =>{
     return new Promise((resolved,rejected)=>{
         setTimeout(()=>{
