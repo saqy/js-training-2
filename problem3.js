5d81162c67690f468ed566abb78733a889295e38
Basit Ali <basitk41@gmail.com>
2021-05-05 18:11:52 +0500

    Update problem3.js

--- problem3.js
+++ problem3.js
@@ -1,5 +1,5 @@
 // Problem 3:- Given an array, rotate the array to the right by k steps, where k is non-negative...
-const input = [5,6,7,2,1];                // input array
+const input = [1,2,3,4,5,6,7];                // input array
 let length = input.length;                // finding length of array     
 let k=4;                                  // number of elements to rotate the right of array
 if(k>0 && !k>length){                                   // checking k should positive
